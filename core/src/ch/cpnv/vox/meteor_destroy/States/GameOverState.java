21c864dd219f36d02cce6d550b2972dee306bb90
Loïc Dessaules <loic.dessaules@cpnv.ch>
2017-06-07 11:11:01 +0200

    Changed level of sounds

--- core/src/ch/cpnv/vox/meteor_destroy/States/GameOverState.java
+++ core/src/ch/cpnv/vox/meteor_destroy/States/GameOverState.java
@@ -69,7 +69,7 @@ public class GameOverState extends State implements InputProcessor{
     private void initAudio() {
         audio = Gdx.audio.newMusic(Gdx.files.internal("audio/game_over.ogg"));
         audio.play();
-        audio.setLooping(true);
+        audio.setLooping(false);
     }
 
     @Override
